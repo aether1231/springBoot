2b0a3268cc23f223a2541501880dada34f38941f
Kevin <tlk999@live.co.uk>
2020-06-24 08:31:00 +0100

    27. Inversion of Control Exercise - added CricketCoach

---
+++ spring-one/src/com/luv2code/springdemo/CricketCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class CricketCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Oh-de-de-dah de-de-de um-ben-bah";
+	}
+
+}
